7bf67d0e2475d775e0ab148e5a40dd11246fb24b
Amoy N <ganymedenick@gmail.com>
2024-03-18 14:50:42 -0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>My Todos</h1>
-       <Todo />
+        <Todo text='learn react' />
+        <Todo  text='master react' />
+        <Todo  text='explore react' />
     </div>
   );
 }
