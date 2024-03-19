1136047ede771a954f8ded30e87ca5596ce1da6a
Amoy N <ganymedenick@gmail.com>
2024-03-19 15:57:28 -0500

    Update meetup app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes} from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavouritesPage from './pages/Favourites';
+import MainNavigation from './components/layout/MainNavigation';
+
 
 function App() {
     // localhost:3000/
@@ -10,6 +12,7 @@ function App() {
 
     return (
         <div>
+            <MainNavigation />
             <Routes>
                 <Route path="/"
                     element={<AllMeetupsPage />} />
