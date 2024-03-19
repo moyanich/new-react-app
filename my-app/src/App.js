e05c5d86272959f45bf067cb6a79893d4e64c8c8
Amoy N <ganymedenick@gmail.com>
2024-03-19 15:26:18 -0500

    Create router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,23 @@
+import { Route, Routes} from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavouritesPage from './pages/Favourites';
 
 function App() {
+    // localhost:3000/
+    // my-page.com
+
     return (
-        <div className="App">
-            
+        <div>
+            <Routes>
+                <Route path="/"
+                    element={<AllMeetupsPage />} />
+                <Route path="/new-meetup" 
+                    element={<NewMeetupPage />} />
+                <Route path="/favourites" 
+                    element={<FavouritesPage />} />
+            </Routes>
         </div>
     );
 }
